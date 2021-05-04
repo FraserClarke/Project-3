66fff6d64589191e6f7ffc637a827e56b176e8d6
Mona Lisa <clarkefraser1@gmail.com>
2021-05-04 21:42:08 +1000

    adding react dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -16,5 +16,24 @@ function App() {
   );
 }
 
+// function App() {
+//   return (
+//     <Router>
+//       <div>
+//         <Navbar />
+//         <Wrapper>
+//           <Route exact path="/" component={About} />
+//           <Route exact path="/about" component={About} />
+//           <Route exact path="/discover" component={Discover} />
+//           <Route exact path="/search" component={Search} />
+//         </Wrapper>
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// }
+
+export default App;
+
 
 export default App;
