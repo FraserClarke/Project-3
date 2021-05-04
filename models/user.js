37ffad7a058c37584e240abbbd892ea47ca631c5
Mona Lisa <clarkefraser1@gmail.com>
2021-05-04 13:53:52 +1000

    Adding boilerplate code for passport, config/models/apiRoutes.

--- models/user.js
+++ models/user.js
@@ -1,3 +1,6 @@
+// Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
+const bcrypt = require("bcryptjs");
+// Creating our User model
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
     firstname: {
@@ -29,5 +32,18 @@ module.exports = (sequelize, DataTypes) => {
       },
     },
   });
-  return User;
+ // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+ User.prototype.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
+// Hooks are automatic methods that run during various phases of the User Model lifecycle
+// In this case, before a User is created, we will automatically hash their password
+User.addHook("beforeCreate", user => {
+  user.password = bcrypt.hashSync(
+    user.password,
+    bcrypt.genSaltSync(10),
+    null
+  );
+});
+return User;
 };
--- server.js
+++ server.js
@@ -1,24 +1,53 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+const session = require("express-session");
+//const PORT = process.env.PORT || 3001;
 const app = express();
+const passport = require("./config/passport");
+// Setting up port and requiring models for syncing
+const PORT = process.env.PORT || 8080;
+const db = require("./models");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+//converts into proper user object
+app.use(passport.session());
+
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+});
 
 // Define API routes here
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+// app.listen(PORT, () => {
+//   console.log(`🌎 ==> API server now on port ${PORT}!`);
+// });
