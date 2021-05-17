dbcf43fad25f50202cf744ffab8414ddc46438d5
Mona Lisa <clarkefraser1@gmail.com>
2021-05-17 18:38:31 +1000

    refining styling for UI

--- client/src/Pages/Main/index.js
+++ client/src/Pages/Main/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import API from "../../utils/API";
-import "./style.css"
+import "./style.css";
 
 export function Main() {
   const history = useHistory();
@@ -11,94 +11,89 @@ export function Main() {
   }
 
   return (
-    <div>
-      You're logged in!
-      <button type="button" onClick={logOut}>
-        Logout
-      </button>
-      <div className="start">
-        <header>
-          <nav className="navigation navbar navbar-default navbar-fixed-top">
-            <div className="container-fluid">
-              <div className="navbar-header">
-                <a className="navbar-brand" href="#">
-                  <h1 className="brand-title"> Country Home </h1>
-                </a>
-              </div>
-              <a href="#">
-                {" "}
-                <p id="menuText" className="navbar-text navbar-right">
-                  Our Story
-                </p>
-              </a>
+    // <div>
+    //   You're logged in!
 
-              <a href="#">
-                <p id="menuText" className="navbar-text navbar-right">
-                  Agents
-                </p>
+    <div className="start">
+      <header>
+        <nav className="navigation navbar navbar-default navbar-fixed-top">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <a className="navbar-brand" href="#">
+                <h1 className="brand-title"> Tradie Tracker </h1>
               </a>
-              <a href="#">
-                <p id="menuText" className="navbar-text navbar-right">
-                  Listings
-                </p>
-              </a>
-              <i
-                id="search"
-                className="navbar-text navbar-right fa fa-search fa-2x"
-              ></i>
             </div>
-          </nav>
-        </header>
-        <body>
-          <div className="intro-header">
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-8">
-                  <div className="intro-message">
-                    <h1 className="animated slideInLeft">
-                      Finding the perfect home, made{" "}
-                      <span className="highlight">simple</span>.
-                    </h1>
-                    <h3 className="animated slideInRight">
-                      Explore your options.
-                    </h3>
+            <a href="#">
+              {" "}
+              <p id="menuText" className="navbar-text navbar-right">
+                Our Story
+              </p>
+            </a>
 
-                    <ul className="list-inline intro-social-buttons">
-                      <li>
-                        <a href="#">
-                          <button className="button animated bounceInUp btn btn-info btn-lg">
-                            {" "}
-                            <span className="network-name">
-                              View Nearby Listings
-                            </span>
-                          </button>
-                        </a>
-                      </li>
+            <a href="#">
+              <p id="menuText" className="navbar-text navbar-right">
+                Agents
+              </p>
+            </a>
+            <a href="#">
+              <p id="menuText" className="navbar-text navbar-right">
+                Listings
+              </p>
+            </a>
+            <button type="button" onClick={logOut}>
+              Logout
+            </button>
+            <i
+              id="search"
+              className="navbar-text navbar-right fa fa-search fa-2x"
+            ></i>
+          </div>
+        </nav>
+      </header>
+      <body>
+        <div className="intro-header">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-8">
+                <div className="intro-message">
+                  <h1 className="animated slideInLeft">
+                    Finding the perfect Trade, made{" "}
+                    <span className="highlight">simple</span>.
+                  </h1>
+                  <h3 className="animated slideInRight">
+                    Explore your options.
+                  </h3>
 
-                      <li>
-                        <a href="#">
-                          <button className="button2 animated bounceInUp btn btn-info btn-lg">
-                            {" "}
-                            <span className="network-name">
-                              Contact an Agent
-                            </span>
-                          </button>
-                        </a>
-                      </li>
-                    </ul>
+                  <ul className="list-inline intro-social-buttons">
+                    <li>
+                      <a href="#">
+                        <button className="button animated bounceInUp btn btn-info btn-lg">
+                          {" "}
+                          <span className="network-name">View</span>
+                        </button>
+                      </a>
+                    </li>
 
-                    <h4 className="learn animated bounceInUp">
-                      About Our Agency
-                    </h4>
-                    <i className="arrow animated infinite pulse fa fa-angle-down icon fa-5x"></i>
-                  </div>
+                    <li>
+                      <a href="#">
+                        <button className="button2 animated bounceInUp btn btn-info btn-lg">
+                          {" "}
+                          <span className="network-name">Request a quote</span>
+                        </button>
+                      </a>
+                    </li>
+                  </ul>
+
+                  <h4 className="learn animated bounceInUp">About</h4>
+                  <i className="arrow animated infinite pulse fa fa-angle-down icon fa-5x"></i>
                 </div>
-                <div className="col-lg-4"></div>
               </div>
+              <div className="col-lg-4"></div>
             </div>
           </div>
-        </body>
-      </div>
+        </div>
+      </body>
     </div>
+    //</div>
   );
 }
--- client/src/Pages/SignUp/index.js
+++ client/src/Pages/SignUp/index.js
@@ -1,74 +1,78 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import API from "../../utils/API"
+import API from "../../utils/API";
 
-function SignUp(){
-    let history = useHistory();
+function SignUp() {
+  let history = useHistory();
 
-    const [formState, setFormState] = useState({
-        email: "",
-        username: "",
-        password: ""
+  const [formState, setFormState] = useState({
+    email: "",
+    username: "",
+    password: "",
+  });
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setFormState({ ...formState, [name]: value.trim() });
+  }
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    if (formState.email && formState.password && formState.username) {
+      API.userSignup({
+        email: formState.email,
+        username: formState.username,
+        password: formState.password,
       })
-    
-    function handleInputChange(event) {
-        const { name, value } = event.target;
-        setFormState({...formState, [name]: value.trim()})
-    };
-
-    function handleFormSubmit(e) {
-        e.preventDefault();
-        if(formState.email && formState.password && formState.username) {
-            API.userSignup({
-                email: formState.email,
-                username: formState.username,
-                password: formState.password
-              })
-                .then((res) => {
-
-                  if(res.status === 200){
-                      history.push("/");
-                  }
-              })
-              .catch(err => console.log(err));
-        
-        }
+        .then((res) => {
+          if (res.status === 200) {
+            history.push("/");
+          }
+        })
+        .catch((err) => console.log(err));
     }
-    
-    return (
-            <form>
-            <input type="text"
-                onChange={handleInputChange}
-                name="email"
-                label="Email"
-                placeholder="Email (required)"
-                required
-                value={formState.email}
-            />
-            <input type="text"
-                onChange={handleInputChange}
-                name="username"
-                label="Username"
-                placeholder="Username (required)"
-                required
-                value={formState.username}
-            />
-            <input type="password"
-                onChange={handleInputChange}
-                name="password"
-                label="Password"
-                placeholder="Password (required)"
-                required
-                value={formState.password}
-            />
-            <button
-                disabled={!(formState.email && formState.password)}
-                onClick={handleFormSubmit}
-            >
-                Create account
-            </button>
-          </form>
-    );
+  }
+
+  return (
+    <div className="col-md-6 col-md-offset-3">
+      <h2>Sign Up Form</h2>
+      <form>
+        <input
+          type="text"
+          onChange={handleInputChange}
+          name="email"
+          label="Email"
+          placeholder="Email (required)"
+          required
+          value={formState.email}
+        />
+        <input
+          type="text"
+          onChange={handleInputChange}
+          name="username"
+          label="Username"
+          placeholder="Username (required)"
+          required
+          value={formState.username}
+        />
+        <input
+          type="password"
+          onChange={handleInputChange}
+          name="password"
+          label="Password"
+          placeholder="Password (required)"
+          required
+          value={formState.password}
+        />
+        <button
+          disabled={!(formState.email && formState.password)}
+          onClick={handleFormSubmit}
+        >
+          Create account
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default SignUp;
